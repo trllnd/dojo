c2a7427e33f64430ad1c93d00a3da1d28be50708
trllnd <trllnd@cbrspc>
2019-03-10 16:12:32 +0100

    refactor : should be 8€ when basket has exactly 1 book copy

--- kata/potter/js/src/index.spec.js
+++ kata/potter/js/src/index.spec.js
@@ -3,8 +3,9 @@
  * @return {number} the price of any conceivable shopping basket, giving as big a discount as possible
  */
 function price(basket) {
+  const unitPrice = 8;
   if (basket.every(book => book === 0)) return 0;
-  if (basket.filter(book => book === 1).length === 1) return 8;
+  if (basket.filter(book => book === 1).length === 1) return unitPrice;
 }
 
 describe("Potter tests", () => {
