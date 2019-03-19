b1f1bdca466be419a463a525970e081102becf19
trllnd <trllnd@cbrspc>
2019-03-19 13:13:55 +0100

    red      : should sum 2-series discounted price

--- kata/potter/js/src/index.spec.js
+++ kata/potter/js/src/index.spec.js
@@ -20,7 +20,11 @@ describe("Potter tests", () => {
     expect(price([0, 0, 4, 0, 0])).toBe(32);
   });
 
-  it("should apply 5% discount for 2 diferent copies", () => {
+  it("should apply 5% discount for 2 different copies", () => {
     expect(price([0, 0, 1, 1, 0])).toBe(16 * 0.95);
+  });
+
+  it("should sum 2-series discounted price", () => {
+    expect(price([0, 2, 0, 2, 0])).toBe(32 * 0.95);
   });
 });
